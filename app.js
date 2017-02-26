d8d8541cfe3565af1bce09ff09cfc8abbafb3a74
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2017-02-26 13:35:18 +0200

    using redux to manage state

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const { createStore, applyMiddleware } = require('redux');
+
 const defaultState = {
   courses: [
     {
@@ -15,8 +17,31 @@ const defaultState = {
   ]
 };
 
+function reducer(state, action) {
+  switch(action.type) {
+    case 'ADD_COURSE':
+      return Object.assign({}, state, {
+        courses: [...state.courses, action.course]
+      });
+    default:
+      return state;
+  }
+}
+
+const logger = store => next => action => {
+  console.log('dispatching ', action);
+  let result = next(action);
+  console.log('state after action ', store.getState());
+  return result;
+}
+
+const store = createStore(reducer, defaultState, applyMiddleware(logger));
+
 function addView(viewFunc) {
   viewFunc(defaultState);
+  store.subscribe(() => {
+    viewFunc(store.getState());
+  });
 }
 
 addView((state) => {
@@ -25,6 +50,14 @@ addView((state) => {
 
 addView((state) => {
   console.log(`The latest course in the library: ${state.courses[state.courses.length -1].name}`);
+});
+
+store.dispatch({
+  type: 'ADD_COURSE',
+  course: {
+    name: 'This is the new course',
+    topic: 'Really does not matter'
+  }
 });
 
 
